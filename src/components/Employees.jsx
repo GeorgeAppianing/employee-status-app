c59511ef0b681838d9ebb00c2058e58a38b11be1
George Appianing <George.Adu.Appianing@gmail.com>
2025-02-14 00:48:18 +0000

    On branch master  Changes to be committed: 	modified:   src/components/Employees.jsx
     Changes not staged for commit:
    	modified:   src/GlobalContext.jsx
    	modified:   src/components/Employees.jsx

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -30,52 +30,56 @@ const Employees = () => {
             </tr>
           </thead>
           <tbody className="bg-white p-6">
-            {displayRows.map((employee) => (
-              <tr key={employee.id} className="text-left hover:bg-gray-100">
-                <td className="flex items-center gap-2 p-2 text-left w-full border-b-1 border-gray-200">
-                  <img
-                    src={employee.avatar}
-                    alt=""
-                    className="h-10 rounded-full"
-                  />
-                  {employee.name}
-                </td>
-                <td className="w-1/5 border-b-1 border-gray-200">
-                  {employee.position}
-                </td>
-                <td className="w-1/5 border-b-1 border-gray-200">
-                  {employee.department}
-                </td>
-                <td className="w-1/5 border-b-1 border-gray-200">
-                  <span
-                    className={`w-full border-b-1 border-gray-200 ${
-                      employee.status === "active"
-                        ? "text-green-300 border-green-200 border-2 rounded-4xl p-2"
-                        : employee.status === "on leave"
-                        ? "text-yellow-300 border-yellow-200 border-2 rounded-4xl p-2"
-                        : employee.status === "inactive"
-                        ? "text-red-300 border-red-200 border-2 rounded-4xl p-2"
-                        : ""
-                    }`}
-                  >
-                    {employee.status}
-                  </span>
-                </td>
-                <td className="w-1/5 border-b-1 border-gray-200">
-                  <button className="text-green-500 hover:text-white hover:bg-green-500 p-1 hover:rounded-full">
-                    <FaRegEye />
-                  </button>
-                  <button className="text-red-500 hover:bg-red-400 hover:text-white p-1 hover:rounded-full">
-                    <MdOutlineDelete
-                      onClick={() => handleDelete(employee.id)}
+            {displayRows.length === 0 ? (
+              <h1>Loading</h1>
+            ) : (
+              displayRows.map((employee) => (
+                <tr key={employee.id} className="text-left hover:bg-gray-100">
+                  <td className="flex items-center gap-2 p-2 text-left w-full border-b-1 border-gray-200">
+                    <img
+                      src={employee.avatar}
+                      alt=""
+                      className="h-10 rounded-full"
                     />
-                  </button>
-                  <button className="text-blue-500 ml-2 hover:bg-blue-400 hover:text-white hover:p-1 hover:rounded-full">
-                    <MdOutlineEdit />
-                  </button>
-                </td>
-              </tr>
-            ))}
+                    {employee.name}
+                  </td>
+                  <td className="w-1/5 border-b-1 border-gray-200">
+                    {employee.position}
+                  </td>
+                  <td className="w-1/5 border-b-1 border-gray-200">
+                    {employee.department}
+                  </td>
+                  <td className="w-1/5 border-b-1 border-gray-200">
+                    <span
+                      className={`w-full border-b-1 border-gray-200 ${
+                        employee.status === "active"
+                          ? "text-green-300 border-green-200 border-2 rounded-4xl p-2"
+                          : employee.status === "on leave"
+                          ? "text-yellow-300 border-yellow-200 border-2 rounded-4xl p-2"
+                          : employee.status === "inactive"
+                          ? "text-red-300 border-red-200 border-2 rounded-4xl p-2"
+                          : ""
+                      }`}
+                    >
+                      {employee.status}
+                    </span>
+                  </td>
+                  <td className="w-1/5 border-b-1 border-gray-200">
+                    <button className="text-green-500 hover:text-white hover:bg-green-500 p-1 hover:rounded-full">
+                      <FaRegEye />
+                    </button>
+                    <button className="text-red-500 hover:bg-red-400 hover:text-white p-1 hover:rounded-full">
+                      <MdOutlineDelete
+                        onClick={() => handleDelete(employee.id)}
+                      />
+                    </button>
+                    <button className="text-blue-500 ml-2 hover:bg-blue-400 hover:text-white hover:p-1 hover:rounded-full">
+                      <MdOutlineEdit />
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
