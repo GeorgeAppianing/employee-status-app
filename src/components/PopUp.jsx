00e3d4b70719114c8be4c1b0f0c559a373c2e520
George Appianing <George.Adu.Appianing@gmail.com>
2025-02-16 00:50:39 +0000

    New changes

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -6,7 +6,7 @@ export const PopUp = () => {
     <div className=" fixed bg-black/50  flex justify-center items-center inset-0  p-6 z-50">
       <h3 className="bg-white p-4">
         Hello
-        <button onClick={TogglePopUp} className="bg-blue-200 p-2">
+        <button onClick={TogglePopUp} className="bg-blue-200 p-4">
           x
         </button>
       </h3>
